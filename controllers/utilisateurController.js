4e8e2db362b39c97bb4bfd5bed8271eaa3c81b7f
Bilal <dev.bilalb@gmail.com>
2024-03-25 21:46:38 +0100

    update usercontroller

--- controllers/utilisateurController.js
+++ controllers/utilisateurController.js
@@ -1,4 +1,59 @@
 const Utilisateur = require('../models/utilisateurModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  try {
+    // Vérifiez si l'utilisateur existe déjà dans la base de données
+    const existingUser = await Utilisateur.findOne({ mail: req.body.mail });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Cet utilisateur existe déjà.' });
+    }
+    
+    // Hash du mot de passe
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    // Créez un nouvel utilisateur
+    const nouvelUtilisateur = new Utilisateur({
+      mail: req.body.mail,
+      password: hashedPassword,
+      prenom: req.body.prenom,
+      nom: req.body.nom,
+      telephone: req.body.telephone
+    });
+
+    // Enregistrez l'utilisateur dans la base de données
+    const utilisateur = await nouvelUtilisateur.save();
+
+    res.status(201).json(utilisateur);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+// Route de connexion
+exports.login = async (req, res) => {
+  try {
+    // Vérifiez si l'utilisateur existe dans la base de données
+    const utilisateur = await Utilisateur.findOne({ mail: req.body.mail });
+    if (!utilisateur) {
+      return res.status(401).json({ message: 'L\'e-mail ou le mot de passe est incorrect.' });
+    }
+
+    // Vérifiez si le mot de passe est correct
+    const passwordMatch = await bcrypt.compare(req.body.password, utilisateur.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'L\'e-mail ou le mot de passe est incorrect.' });
+    }
+
+    // Générez un token JWT pour l'utilisateur authentifié
+    const token = jwt.sign({ mail: utilisateur.mail, prenom: utilisateur.prenom, nom: utilisateur.nom, telephone: utilisateur.telephone, userId: utilisateur._id }, 'votre_clé_secrète', { expiresIn: '1h' });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
 
 // Créer un nouvel utilisateur
 exports.createUtilisateur = async (req, res) => {
